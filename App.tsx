7a706ce864802a6ad738b8b906a0d79343ab802f
Arthur <alphacosmos776@gmail.com>
2024-06-01 20:08:43 -0300

    decimo commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import { NavegacaoPrincipal } from './src/navigation';
 import { initializeApp } from '@firebase/app';
-import { auth, db } from './src/config/firebase-config';
+import { firebaseConfig } from './src/config/firebase-config';
 import {  initializeAuth, getReactNativePersistence } from '@firebase/auth/react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 
 export default function App() {
+
+  const app = initializeApp(firebaseConfig)
+  initializeAuth(app, { persistence:  getReactNativePersistence(AsyncStorage)})
+  
   return (
     <NavegacaoPrincipal/>
   );
